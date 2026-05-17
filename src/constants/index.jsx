d5710bdafaabc2ceac093060ba116939e8e24556
Ahsan Gulzar <162182753+ahsanbhutta01@users.noreply.github.com>
2026-05-17 17:25:41 +0500

    .

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -3,7 +3,7 @@ import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
-import project6 from "../assets/project6.webp";
+import project6 from "../assets/project6.png";
 import personImage from "../assets/testimonial.jpg";
 
 
